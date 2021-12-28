0354888a986eceda0140626e4952e7986b765887
Kungger-git <matthewriel63@gmail.com>
2021-12-28 13:25:49 +0800

    Change string formatting in each method

--- modules/convert_string.py
+++ modules/convert_string.py
@@ -4,17 +4,20 @@ import time
 class StringConverter:
     """ Convert/Format plural words, time & bytes """
 
-    def pluralS(self, v):
+    @staticmethod
+    def pluralS(v):
         return 's' if not abs(v) == 1 else ''
 
-    def convertTime(self, seconds):
+    @staticmethod
+    def convertTime(seconds):
         return time.strftime("%H:%M:%S", time.gmtime(seconds))
 
-    def formatBytes(self, size):
+    @staticmethod
+    def formatBytes(size):
         power = 2**10
         n = 0
         power_labels = {0: '', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
         while size > power:
             size /= power
             n += 1
-        return f'{round(size, 2)}{power_labels[n]}b'
+        return "{}{}b".format(round(size, 2), power_labels[n])
