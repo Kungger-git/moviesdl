ee95785bbd3cce8b533554e5eb3db64416e1037b
Kungger-git <matthewriel63@gmail.com>
2021-12-28 15:42:39 +0800

    Add run_once wrapper

---
+++ modules/run_once.py
@@ -0,0 +1,14 @@
+from functools import wraps
+
+def run_once(f):
+    """Runs a function (successfully) only once.
+    The running can be reset by setting the `has_run` attribute to False
+    """
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if not wrapper.has_run:
+            result = f(*args, **kwargs)
+            wrapper.has_run = True
+            return result
+    wrapper.has_run = False
+    return wrapper
